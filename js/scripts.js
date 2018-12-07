e70bf5d206cc2cfba85a0cf8f64339b2a3d0f064
Alex Kiagiri <akiagiri@gmail.com>
2018-12-07 20:13:26 +0300

    Added bootstrap.css files and also added logic to check for correct answers

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,60 @@
 $(document).ready(function() {
   
-  // $("button#green").click(function() {
-  //   $("body").css("background-color", "green");
-  // });
+  var total = 0;
 
-  // $("button#yellow").click(function() {
-  //   $("body").css("background-color", "yellow");
-  // });
-  // $("button#red").click(function() {
-  //   $("body").css("background-color", "red");
-  // });
-  
   $("button#green").click(function() {
-    $("body").removeClass();
-    $("body").addClass("green-background");
+
+    if (!$('[name="q1"]').is(':checked') || !$('[name="q2"]').is(':checked') || !$('[name="q3"]').is(':checked') || !$('[name="q4"]').is(':checked') || !$('[name="q5"]').is(':checked')){
+
+      alert("Kindly answer all questions!");
+
+   }
+  else{
+
+      if ($('[name="q1"]').is(':checked')){
+        
+        if ($('[name="q1"]').val() === 'script') {
+          total += 10;
+        }
+      }
+
+      if ($('[name="q2"]').is(':checked')){
+         
+        if ($('[name="q2"]').val() === 'java-script') {
+          total += 10;
+        }
+       
+      }
+
+      if ($('[name="q3"]').is(':checked')){
+
+        if ($('[name="q3"]').val() === 'html') {
+          total += 10;
+        }
+       
+      }
+
+      if ($('[name="q4"]').is(':checked')){
+
+        if ($('[name="q4"]').val() === 'container') {
+          total += 10;
+        }
+      }
+
+      if ($('[name="q5"]').is(':checked')){
+
+        if ($('[name="q5"]').val() === 'cascading') {
+          total += 10;
+        }
+      }
+
+      console.log(total);
+
+      alert(total);
+
+  }
+
+
   });
 
-  $("button#yellow").click(function() {
-    $("body").removeClass();
-    $("body").addClass("yellow-background");
-  });
-
-  $("button#red").click(function() {
-    $("body").removeClass();
-    $("body").addClass("red-background");
-  });
-  
 }); 
