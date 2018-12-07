618dde2673199c962d98d7392d12a2cccb3e0fcd
Alex Kiagiri <akiagiri@gmail.com>
2018-12-07 08:39:48 +0300

    Added a JavaScript folder and scripts.js and jquery-3.3.1.js files

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  
+  // $("button#green").click(function() {
+  //   $("body").css("background-color", "green");
+  // });
+
+  // $("button#yellow").click(function() {
+  //   $("body").css("background-color", "yellow");
+  // });
+  // $("button#red").click(function() {
+  //   $("body").css("background-color", "red");
+  // });
+  
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  
+}); 
