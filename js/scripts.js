c9ff9392dfd1fa03f5ae6b4cb75bf568e6ce2354
Alex Kiagiri <akiagiri@gmail.com>
2018-12-08 18:17:04 +0300

    Limited scope of question variables.

--- js/scripts.js
+++ js/scripts.js
@@ -15,27 +15,27 @@ $(document).ready(function() {
 
     else{
 
-        q1 = $("input[name='q1']:checked").val();
+        var q1 = $("input[name='q1']:checked").val();
         if (q1 === 'script') {
             total += 10;
         }
 
-        q2 = $("input[name='q2']:checked").val();
+        var q2 = $("input[name='q2']:checked").val();
         if (q2 === 'java-script') {
             total += 10;
         }
       
-        q3 = $("input[name='q3']:checked").val();
+        var q3 = $("input[name='q3']:checked").val();
         if (q3 === 'html') {
             total += 10;
         }
         
-        q4 = $("input[name='q4']:checked").val();
+        var q4 = $("input[name='q4']:checked").val();
         if (q4 === 'container') {
             total += 10;
         }
 
-        q5 = $("input[name='q5']:checked").val();
+        var q5 = $("input[name='q5']:checked").val();
         if (q5 === 'cascading') {
             total += 10;
         }
